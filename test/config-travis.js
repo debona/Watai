db33dcc0a16c379ec420bf111c64de64636c62c1
Thomas DE BONA <thomas.debona@mesh-md.fr>
2013-12-20 16:10:45 +0100

    Run the test suite with firefox on travis

---
+++ test/config-travis.js
@@ -0,0 +1,11 @@
+module.exports = {
+	seleniumServerURL: 'http://127.0.0.1:4444/wd/hub',
+	baseURL: 'file://' + __dirname + '/resources/page.html',
+	driverCapabilities: {
+		browserName: 'firefox',
+		javascriptEnabled: true
+	},
+	quit: 'never',
+	browserWarmupTimeout: 30 * 1000,	//ms
+	timeout: 500,	// implicit wait for elements lookup in milliseconds, has to be lower than mocha's timeout to test for missing elements rejection
+}
